e128fdd9df6c1385c80613c19d32404bff233ec2
Krish <krag10706@gmail.com>
2026-08-15 09:26:21 +0000

    Testing V7

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-'
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -7,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Load Tester API V6 is running"
+        "message": "Load Tester API V7 is running"
     })
 
 
